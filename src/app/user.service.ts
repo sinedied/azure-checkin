c9fa20f6631fa698fa0d8b70d15c92f39040c671
sinedied <noda@free.fr>
2021-06-16 21:10:29 +0200

    feat: add user service

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Optional } from '@angular/core';
+import { UserInfo } from './user-info';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  constructor() {}
+
+  async getUserInfo(): Promise<UserInfo|null> {
+    try {
+      const response = await fetch('/.auth/me');
+      const payload = await response.json();
+      const { clientPrincipal } = payload;
+      return clientPrincipal;
+    } catch (error) {
+      return null;
+    }
+  }
+
 }
