0807c48228dda42f05a92004f3ed149f03cc4621
sinedied <noda@free.fr>
2021-06-18 23:42:19 +0200

    fix: pass attribution

--- api/pass-get/index.js
+++ api/pass-get/index.js
@@ -1,7 +1,10 @@
-module.exports = async function (context, req, event) {
+module.exports = async function (context, req, event, client) {
   const eventId = req.params.eventId;
 
-  if (!eventId || eventId !== 'SWC210622') {
+  context.log({client})
+
+  if (!eventId || !event) {
+    context.log(`Event not found, id=${eventId}`);
     return { status: 404, body: 'Not found' };
   }
 
@@ -14,34 +17,47 @@ module.exports = async function (context, req, event) {
   if (!userInfo || !userInfo.userId) {
     return { status: 401, body: 'Unauthorized' };
   }
+  
+  const { userId } = userInfo;
 
   context.log(`Requesting pass for user ${userId}`);
 
   // Get user pass, if not assigned then assign it and return it
-  let userPass = Object.keys(
-    event.passes.find((pass) => event.passes[pass] === userInfo.userId)
+  let userPass = Object.keys(event.passes).find(
+    (pass) => event.passes[pass] === userId
   );
 
-  if (userPass) {
-    return { pass: userPass };
-  } else {
+  if (!userPass) {
     context.log(`User ${userId} has not yet a pass attributed`);
 
-    userPass = Object.keys(
-      event.passes.find((pass) => event.passes[pass] === null)
+    userPass = Object.keys(event.passes).find(
+      (pass) => event.passes[pass] === null
     );
-    event.passes[userPass] = userInfo.userId;
 
-    context.log(`Attributed pass ${userPass} to user ${userId}`);
-
-    try {
-      await event.replace(event, {
-        accessCondition: { type: 'IfMatch', condition: event._etag },
-      });
-    } catch (err) {
-      return { status: 409, body: 'Conflict' };
+    if (!userPass) {
+      context.log(`No free pass available`);
+      return { status: 404, body: 'Not found' };
     }
 
-    return { pass: userPass };
+    event.passes[userPass] = userId;
+
+    context.log(`Attributed pass ${userPass} to user ${userId}`);
+    context.bindings.updatedEvent = JSON.stringify(event);
+
+    // try {
+    //   await event.replace(event, {
+    //     accessCondition: { type: 'IfMatch', condition: event._etag },
+    //   });
+    // } catch (err) {
+    //   context.log(err);
+    //   context.log(`Event etag ${event._etag} does not match!`);
+    //   return { status: 409, body: 'Conflict' };
+    // }
+  } else {
+    context.log(`Found attributed pass ${userPass}`);
   }
+
+  return {
+    body: { pass: userPass },
+  };
 };
