8ca5668ca35547213e998c5a3852c679a4a46fb3
sinedied <noda@free.fr>
2021-07-30 14:54:54 +0200

    feat: expose more data from API

--- api/helpers/event.js
+++ api/helpers/event.js
@@ -1,8 +1,11 @@
-const createEvent = (data) => ({
+const createEvent = (data, withPasses = false) => ({
   id: data.id,
   name: data.name,
   date: data.date,
   owner: data.owner,
+  usedPasses: Object.values(data.passes).filter(hash => hash).length,
+  totalPasses: Object.values(data.passes).length,
+  ...(withPasses && data.passes || []),
 });
 
 module.exports = {
