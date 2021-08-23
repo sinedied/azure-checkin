18a6d724675de796b2941b3eb240ce522161ab00
sinedied <noda@free.fr>
2021-08-23 20:58:09 +0200

    style: redesign no pass

--- src/app/event/no-pass-card.component.ts
+++ src/app/event/no-pass-card.component.ts
@@ -4,12 +4,38 @@ import { Component, Input } from '@angular/core';
   selector: 'app-no-pass-card',
   template: `
     <mat-card>
-      <h1>{{ eventName }}</h1>
-      <p>Sorry, no more passes are available for this event.</p>
-      <h2>:-(</h2>
+      <mat-card-header>
+        <img mat-card-avatar src="./assets/azure.svg" alt="Azure Logo" />
+        <mat-card-title class="text-ellipsis">
+          {{ eventName }}
+        </mat-card-title>
+        <mat-card-subtitle>Check-in error</mat-card-subtitle>
+      </mat-card-header>
+      <p class="sad">
+        <b>Sorry, this flight is fully booked, there's no seat available.</b>
+        <mat-icon inline>sentiment_very_dissatisfied</mat-icon>
+      </p>
       <p>Please contact the event organizer for more information.</p>
     </mat-card>
   `,
+  styles: [
+    `
+      .sad {
+        > .mat-icon {
+          font-size: 3em;
+          line-height: 1em;
+          vertical-align: middle;
+          margin-left: 0.5rem;
+        }
+      }
+      .mat-card-avatar {
+        border-radius: 0;
+      }
+      .error {
+        color: red;
+      }
+    `,
+  ],
 })
 export class NoPassCardComponent {
   @Input() eventName: string = '';
