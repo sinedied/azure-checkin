949c7c33a1e1614db2438850c7f96f6e92bcbc1d
sinedied <noda@free.fr>
2021-08-25 17:07:14 +0200

    feat: allow getting event passes

--- api/helpers/auth.js
+++ api/helpers/auth.js
@@ -1,11 +1,11 @@
 const administrators = require('../administrators.json');
 
 function getUserInfo(req) {
-  const token = Buffer.from(
-    req.headers['x-ms-client-principal'],
-    'base64'
-  ).toString('ascii');
   try {
+    const token = Buffer.from(
+      req.headers['x-ms-client-principal'],
+      'base64'
+    ).toString('ascii');
     return (token && JSON.parse(token)) || null;
   } catch (error) {
     return null;
