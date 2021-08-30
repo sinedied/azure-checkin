e22c5ff8e26766517fe512182bfa8263ab8d72b1
sinedied <noda@free.fr>
2021-08-30 17:00:27 +0200

    feat: add manual pass allocation/deallocation

---
+++ api/pass-patch/index.js
@@ -0,0 +1,45 @@
+const { getUserInfo, isAdmin } = require('../helpers/auth');
+
+module.exports = async function (context, req, event) {
+  const eventId = req.params.eventId;
+  const pass = req.params.pass;
+  const body = req.body;
+
+  if (!eventId || !event) {
+    context.log(`Event not found, id=${eventId}`);
+    return { status: 404, body: 'Not found' };
+  }
+
+  if (event.passes[pass] === undefined) {
+    context.log(`Pass not found, pass=${pass}`);
+    return { status: 404, body: 'Not found' };
+  }
+
+  const userInfo = getUserInfo(req);
+  const { userDetails } = userInfo || {};
+
+  if (!userInfo || !userInfo.userId) {
+    return { status: 401, body: 'Unauthorized' };
+  }
+
+  if (!isAdmin(userDetails, event)) {
+    return { status: 403, body: 'Forbidden' };
+  }
+
+  if (!body || body.assign === undefined) {
+    return { status: 400, body: 'Body is missing "assign" property' };
+  }
+
+  if (Boolean(body.assign)) {
+    context.log(`Attributing pass ${pass} manually`);
+    event.passes[pass] = '__manually_assigned';
+  } else {
+    context.log(`Freeing pass ${pass} attribution`);
+    event.passes[pass] = null;
+  }
+
+  context.log(`Update pass ${pass} attribution`);
+  context.bindings.updatedEvent = JSON.stringify(event);
+
+  return { status: 204 };
+};
