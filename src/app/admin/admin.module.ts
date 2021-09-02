1c7f413be06fdb05a1d858eed7f2ab3553599dc7
sinedied <noda@free.fr>
2021-09-02 16:49:12 +0200

    fix: use current locale for dates

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -6,9 +6,11 @@ import { EventListComponent } from './event-list.component';
 import { EventEditComponent } from './event-edit.component';
 import { LocalDatePipe } from './local-date.pipe';
 import { UnauthorizedComponent } from './unauthorized.component';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
 
 @NgModule({
   declarations: [AdminComponent, EventListComponent, EventEditComponent, LocalDatePipe, UnauthorizedComponent],
   imports: [SharedModule, AdminRoutingModule],
+  providers: [{ provide: MAT_DATE_LOCALE, useValue: navigator.language || 'en-US' }],
 })
 export class AdminModule {}
