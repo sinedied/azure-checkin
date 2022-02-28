96beada640e93cf46ed65a4523834cf29b605c4e
sinedied <noda@free.fr>
2022-02-28 10:16:46 +0100

    refactor: lint fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 import { env } from './.env';
 
 export const environment = {
-  version: env.npm_package_version + '-dev',
+  version: env['npm_package_version'] + '-dev',
   production: false,
 };
 
